61cd7f6c67c455d22221073cc13c4f65a5bae66c
ranaagouda <ranagouda01@gmail.com>
2025-02-20 18:00:51 +0200

    todoApp with ReactNative

---
+++ styles.js
@@ -0,0 +1,66 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    padding: 20,
+    margin: 20,
+  },
+  header: {
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  inputContainer: {
+    marginBottom: 20,
+  },
+  input: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+  },
+  submitButton: {
+    backgroundColor: "teal",
+    paddingVertical: 10,
+    alignItems: "center",
+  },
+  submitButtonText: {
+    color: "white",
+    fontWeight: "bold",
+  },
+  filterContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginBottom: 20,
+  },
+  filterButton: {
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: "gray",
+  },
+  selectedFilterButton: {
+    backgroundColor: "lightgray",
+  },
+  filterButtonText: {
+    fontWeight: "bold",
+  },
+  taskItem: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "lightgray",
+  },
+  logo: {
+    width: 66,
+    height: 58,
+    alignSelf: "center",
+    marginVertical: 10,
+  },
+});
